93014912afd8e52f33aa1bf8241ccb2e4ddc1d78
Axel Herrera <axelh260498@gmail.com>
2024-11-18 22:04:54 -0300

    Creating Hero Section

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,29 @@
+import video1 from "../assets/video1.mp4";
+import video2 from "../assets/video2.mp4";
+const HeroSection = () => {
+  return (
+    <div className="flex flex-col items-center mt-6 lg:t-20">
+        <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide">
+            VirtualR build tools 
+            
+            <span className="bg-gradient-to-r from-orange-500 to-orange-800 bg-clip-text text-transparent">
+               {" "} for developers
+            </span>
+        </h1>
+        <p className="mt-10 text-lg text-center text-neutral-500 max-w-4xl">Empower your creativity and bring your VR app ideas to life with our
+        intuitive development tools. Get started today and turn your imagination
+        into immersive reality!
+        </p>
+        <div className="flex justify-center my-10">
+            <a href="#" className="bg-gradient-to-r from-orange-500 to-orange-800 py-3 px-4 mx-3 rounded-md">
+                Start for free
+            </a>
+            <a href="#" className="py-3 px-4 mx-3 border rounded-md">
+                Documentation
+            </a>
+        </div>
+    </div>
+  )
+}
+
+export default HeroSection
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,25 @@
+import { useState } from "react";
+// ICONOS DE NAVBAR PARA MOBILE
+import {Menu , X} from "lucide-react";
+
+// IMAGENES DE NAVBAR 
 import logo from "../assets/logo.png";
 import {navItems} from "../assets/constants";
 
+
 const Navbar = () => {
+    const [mobileDrawerOpen, setMobileDrawerOpen] = useState(false);
+
+    const toggleNavbar = () => {
+        setMobileDrawerOpen(!mobileDrawerOpen);
+    };
+
   return (
     <nav className="stycky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80">
         <div className="container px-4 mx-auto relative text-sm">
             <div className="flex justify-between items-center">
                 <div className="flex items-center flex-shirnk-0">
-                    <img className="h-10 w-10 mr-2" src="{ logo }" alt="logo" />
+                    <img className="h-10 w-10 mr-2" src="{logo}" alt="logo" />
                     <span className="text-xl-tracking-tight">VirtualR</span>
                 </div>
                 <ul className="hidden lg:flex ml-14 space-x-12">
@@ -25,7 +37,32 @@ const Navbar = () => {
                         Create an account
                     </a>
                 </div>
+                
+                <div className="lg:hidden md:flex-col justify-end">
+                    <button onClick={toggleNavbar}>
+                        { mobileDrawerOpen ? <X /> : <Menu />}                 
+                    </button>
+                </div>
             </div>
+            {mobileDrawerOpen && (
+                <div className="fixed right-0 z-20 bg-neutral-900 w-full p-12 flex flex-col justify-center items-center lg:hidden">
+                    <ul>
+                        {navItems.map((item, index) => (
+                            <li key={index} className="py-4">
+                                <a href={item.href}>{item.label}</a>    
+                            </li>   
+                        ))}          
+                    </ul>
+                    <div className="flex space-x-6">
+                        <a href="#" className="py-2 px-3 border rounded-md">
+                            Sign In
+                        </a>
+                        <a href="#" className="py-2 px-3 rounded-mb bg-gradient-to-r from-orange-500 to-orange-800">
+                            Create an account
+                        </a>
+                    </div>
+                </div>  
+            )}
         </div>
     </nav>
   )
