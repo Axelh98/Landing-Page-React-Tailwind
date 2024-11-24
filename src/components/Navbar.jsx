080cf1d8f637035208fefa015925b629e18692eb
Axel Herrera <axelh260498@gmail.com>
2024-11-24 20:38:28 -0300

    Workflow Section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
         <div className="container px-4 mx-auto relative text-sm">
             <div className="flex justify-between items-center">
                 <div className="flex items-center flex-shirnk-0">
-                    <img className="h-10 w-10 mr-2" src="{logo}" alt="logo" />
+                    <img className="h-10 w-10 mr-2" src={logo} alt="logo" />
                     <span className="text-xl-tracking-tight">VirtualR</span>
                 </div>
                 <ul className="hidden lg:flex ml-14 space-x-12">
--- src/components/Workflow.jsx
+++ src/components/Workflow.jsx
@@ -1,12 +1,33 @@
 import { CheckCircle2 } from "lucide-react";
-
+import codeImg from "../assets/img/code.jpg";
+import { checklistItems } from "../assets/constants";
 
 const Workflow = () => {
   return (
     <div className="mt-20">
-        <h2 className="text-3xl sm:text-5xl lg:text-6xl text">Accelerate your coding workflow</h2>
-    </div>
-  )
-}
+      <h2 className="text-3xl sm:text-5xl lg:text-6xl text">
+        Accelerate your{" "}
+        <span className="bg-gradient-to-r from-orange-500 to-pink-800 bg-clip-text text-transparent">
+          coding workflow
+        </span>
+      </h2>
 
-export default Workflow
+      <div className="flex flex-wrap justify-center">
+        <div className="p-2 w-full lg:w-1/2">
+          <img src={ codeImg } alt="Code" />
+        </div>
+        <div className="pt-12 w-full lg:w-1/2">
+          {checklistItems.map((item, index) => (
+            <div key={index} className="flex mb-12">
+              <div className="text-green-400 mx-6 bg-neutral-900 h-10 w-10 p-2 justify-center items-center rounder-full">
+                <CheckCircle2 />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Workflow;
